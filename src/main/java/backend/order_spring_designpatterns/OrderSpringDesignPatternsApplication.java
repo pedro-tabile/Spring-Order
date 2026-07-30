1228bc18e1b84026211fb8d31308d74479e0a3f1
Pedro364898 <tabilepedro1903@gmail.com>
2026-07-30 11:38:21 -0300

    commit inicial

---
+++ src/main/java/backend/order_spring_designpatterns/OrderSpringDesignPatternsApplication.java
@@ -0,0 +1,13 @@
+package backend.order_spring_designpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderSpringDesignPatternsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderSpringDesignPatternsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/backend/order_spring_designpatterns/OrderSpringDesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.order_spring_designpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderSpringDesignPatternsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
