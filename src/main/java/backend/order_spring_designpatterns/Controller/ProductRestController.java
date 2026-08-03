6d942c6f93de1387ee61df47c76d2a055a93dec2
Pedro364898 <tabilepedro1903@gmail.com>
2026-08-03 20:24:23 -0300

    Criação do controller para Product

---
+++ src/main/java/backend/order_spring_designpatterns/Controller/ProductRestController.java
@@ -0,0 +1,50 @@
+package backend.order_spring_designpatterns.Controller;
+
+import backend.order_spring_designpatterns.Entity.Product;
+import backend.order_spring_designpatterns.Service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("produtos")
+// Classe responsável pelo controle de requisições e respostas da API para operações com produtos
+public class ProductRestController {
+    @Autowired
+    private ProductService productService;
+
+    // Annotations Mapping permitem tratar métodos HTTP como PUT, DELETE, CREATE e UPDATE.
+    @GetMapping
+    public ResponseEntity<Iterable<Product>> findAll(){
+        // A classe ResponseEntity representa a resposta HTTP inteira (status e corpo) enviada ao cliente após requisição
+        return ResponseEntity.ok(productService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> findById(@PathVariable Long id){
+        return ResponseEntity.ok(productService.findById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> update(@RequestBody Product product, @PathVariable Long id){
+        return ResponseEntity.ok(productService.update(product, id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Product> delete(@PathVariable Long id){
+        productService.delete(id);
+        return ResponseEntity.ok().build(); // Retorna resposta sem corpo
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> delete(@RequestBody Product product){
+        return ResponseEntity.ok(productService.insert(product));
+    }
+}
