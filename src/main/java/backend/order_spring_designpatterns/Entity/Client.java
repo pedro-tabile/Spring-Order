90e30f86724a35ae6e6e6066bb9d4fab0370c9fc
Pedro364898 <tabilepedro1903@gmail.com>
2026-08-03 16:31:05 -0300

    Inserção de service para Client e comentários no Main abordando sobre os Singleton no projeto.

--- src/main/java/backend/order_spring_designpatterns/Entity/Client.java
+++ src/main/java/backend/order_spring_designpatterns/Entity/Client.java
@@ -2,13 +2,16 @@ package backend.order_spring_designpatterns.Entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 
 // Definição da entidade Client (Cliente)
 @Entity
 public class Client {
+    // Chave primária com auto-incremento
     @Id
+    @GeneratedValue()
     private Long id;
 
     // Não permite campo vazio ou somente com espaços
@@ -19,4 +22,8 @@ public class Client {
     @NotBlank
     @Column(nullable = false)
     private String email;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
