d82aa60852c151bf0cbcf9fabd8a1629b47ff76a
Pedro364898 <tabilepedro1903@gmail.com>
2026-09-08 15:56:18 -0300

    Inserção de chaves (id e secret) da API e início do SendPulseService

--- src/main/java/backend/order_spring_designpatterns/Client/SendPulseClient.java
+++ src/main/java/backend/order_spring_designpatterns/Client/SendPulseClient.java
@@ -4,29 +4,17 @@ import backend.order_spring_designpatterns.DTO.Request.AccessTokenRequestDTO;
 import backend.order_spring_designpatterns.DTO.Request.EmailRequestDTO;
 import backend.order_spring_designpatterns.DTO.Response.AccessTokenResponseDTO;
 import backend.order_spring_designpatterns.DTO.Response.EmailResponseDTO;
-import backend.order_spring_designpatterns.Service.SendPulseService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "send-email-smtp", url = "https://api.sendpulse.com/")
-// Classe que representa um Client para consumo da api externa
-public class SendPulseClient {
-    @Autowired
-    private SendPulseService sendPulseService;
-
+// Interface que representa um Client para consumo da api externa - a implementação é realizada pelo Spring
+public interface SendPulseClient {
     @PostMapping(value = "oauth/access_token")
-    AccessTokenResponseDTO getAuthorization(@RequestBody AccessTokenRequestDTO accessTokenRequestDTO){
-
-        return null;
-    };
+    AccessTokenResponseDTO getAuthorization(@RequestBody AccessTokenRequestDTO accessTokenRequestDTO);
 
     @PostMapping(value = "smtp/emails")
-    EmailResponseDTO sendEmail(@RequestBody EmailRequestDTO emailRequestDTO,
-                               @RequestHeader("Authorization") String token){
-
-        return null;
-    };
+    EmailResponseDTO sendEmail(@RequestBody EmailRequestDTO emailRequestDTO, @RequestHeader("Authorization") String token);
 }
--- src/main/java/backend/order_spring_designpatterns/Service/SendPulseService.java
+++ src/main/java/backend/order_spring_designpatterns/Service/SendPulseService.java
@@ -1,9 +1,33 @@
 package backend.order_spring_designpatterns.Service;
 
+import backend.order_spring_designpatterns.Client.SendPulseClient;
+import backend.order_spring_designpatterns.DTO.Request.AccessTokenRequestDTO;
+import backend.order_spring_designpatterns.DTO.Request.EmailRequestDTO;
+import backend.order_spring_designpatterns.DTO.Response.AccessTokenResponseDTO;
+import backend.order_spring_designpatterns.DTO.Response.EmailResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 /* Classe que define o envio de emails */
 @Service
 public class SendPulseService {
+    @Autowired
+    private SendPulseClient sendPulseClient;
 
+    @Value("${SENDPULSE_CLIENT_SECRET}")
+    private String clientSecret;
+
+    @Value("${SENDPULSE_CLIENT_ID}")
+    private String clientId;
+
+    public AccessTokenResponseDTO getAccessToken() {
+        AccessTokenRequestDTO dataRequest = new AccessTokenRequestDTO("client_credentials", clientId, clientSecret);
+        return sendPulseClient.getAuthorization(dataRequest);
+    }
+
+    public EmailResponseDTO sendEmail(EmailRequestDTO emailRequestDTO) {
+        AccessTokenResponseDTO authorization = getAccessToken();
+        return sendPulseClient.sendEmail(emailRequestDTO, "Bearer " + authorization.access_token());
+    }
 }
